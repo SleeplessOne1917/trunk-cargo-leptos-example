5399f6c59f59f882ddca7914017aade25c6e93cd
SleeplessOne1917 <28871516+SleeplessOne1917@users.noreply.github.com>
2024-04-25 18:35:23 -0400

    Trim the fat

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,6 @@ pub fn hydrate() {
     use crate::counters::*;
     use leptos::*;
 
-    _ = console_log::init_with_level(log::Level::Debug);
     console_error_panic_hook::set_once();
 
     mount_to_body(Counters);
--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,64 @@
 mod counters;
-
 use crate::counters::*;
-use actix_files::Files;
-use actix_web::*;
-use leptos::*;
-use leptos_actix::{generate_route_list, LeptosRoutes};
 
-#[get("/api/events")]
-async fn counter_events() -> impl Responder {
-    use crate::counters::ssr_imports::*;
-    use futures::StreamExt;
+cfg_if::cfg_if! {
+    if #[cfg(feature = "ssr")] {
+        use actix_files::Files;
+        use actix_web::*;
+        use leptos::*;
+        use leptos_actix::{generate_route_list, LeptosRoutes};
 
-    let stream = futures::stream::once(async {
-        crate::counters::get_server_count().await.unwrap_or(0)
-    })
-    .chain(COUNT_CHANNEL.clone())
-    .map(|value| {
-        Ok(web::Bytes::from(format!(
-            "event: message\ndata: {value}\n\n"
-        ))) as Result<web::Bytes>
-    });
-    HttpResponse::Ok()
-        .insert_header(("Content-Type", "text/event-stream"))
-        .streaming(stream)
-}
+        #[get("/api/events")]
+        async fn counter_events() -> impl Responder {
+            use crate::counters::ssr_imports::*;
+            use futures::StreamExt;
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    // Setting this to None means we'll be using cargo-leptos and its env vars.
-    // when not using cargo-leptos None must be replaced with Some("Cargo.toml")
-    let conf = get_configuration(None).await.unwrap();
+            let stream = futures::stream::once(async {
+                crate::counters::get_server_count().await.unwrap_or(0)
+            })
+            .chain(COUNT_CHANNEL.clone())
+            .map(|value| {
+                Ok(web::Bytes::from(format!(
+                    "event: message\ndata: {value}\n\n"
+                ))) as Result<web::Bytes>
+            });
 
-    let addr = conf.leptos_options.site_addr;
-    let routes = generate_route_list(Counters);
+            HttpResponse::Ok()
+                .insert_header(("Content-Type", "text/event-stream"))
+                .streaming(stream)
+        }
 
-    HttpServer::new(move || {
-        let leptos_options = &conf.leptos_options;
-        let site_root = &leptos_options.site_root;
+        #[actix_web::main]
+        async fn main() -> std::io::Result<()> {
+            // Setting this to None means we'll be using cargo-leptos and its env vars.
+            // when not using cargo-leptos None must be replaced with Some("Cargo.toml")
+            let conf = get_configuration(None).await.unwrap();
 
-        App::new()
-            .service(counter_events)
-            .leptos_routes(
-                leptos_options.to_owned(),
-                routes.to_owned(),
-                Counters,
-            )
-            .service(Files::new("/", site_root))
-        //.wrap(middleware::Compress::default())
-    })
-    .bind(&addr)?
-    .run()
-    .await
+            let addr = conf.leptos_options.site_addr;
+            let routes = generate_route_list(Counters);
+
+            HttpServer::new(move || {
+                let leptos_options = &conf.leptos_options;
+                let site_root = &leptos_options.site_root;
+
+                App::new()
+                    .service(counter_events)
+                    .leptos_routes(
+                        leptos_options.to_owned(),
+                        routes.to_owned(),
+                        Counters,
+                )
+                .service(Files::new("/", site_root))
+                //.wrap(middleware::Compress::default())
+            })
+            .bind(&addr)?
+            .run()
+            .await
+        }
+    }  else {
+        fn main() {
+            console_error_panic_hook::set_once();
+            leptos::mount_to_body(Counters);
+        }
+    }
 }
